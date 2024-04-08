95086b5bbe9cba617dc5e30d032412a44c4cec1f
SUMMER\quanh <quanhavo005@gmail.com>
2024-04-09 00:18:49 +0700

    refactor code home screen

---
+++ src/screens/HomeScreen/useHomeLayout.ts
@@ -0,0 +1,62 @@
+import { useIsFocused } from "@react-navigation/native";
+import { useEffect, useRef, useState } from "react";
+import { Animated } from "react-native";
+
+export const useHomeLayout = () => {
+    const isFocus = useIsFocused();
+
+    const [isOpenSideBar, setIsOpenSidebar] = useState<boolean>(true);
+
+    const sidebarAniValue = useRef(new Animated.Value(0)).current;
+
+    useEffect(() => {
+        //close sidebar with delay when navigate to other screen
+        let timer = null;
+        if (!isFocus && !isOpenSideBar) {
+            timer = setTimeout(() => {
+                toggleOpenSideBar();
+            }, 200);
+        }
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [isFocus, isOpenSideBar]);
+
+    const toggleOpenSideBar = () => {
+        Animated.timing(sidebarAniValue, {
+            toValue: isOpenSideBar ? 1 : 0,
+            duration: 250,
+            useNativeDriver: false,
+        }).start();
+        setIsOpenSidebar(!isOpenSideBar);
+    };
+
+    const interpolatedTop = sidebarAniValue.interpolate({
+        inputRange: [0, 1],
+        outputRange: ['0%', '15%'],
+    });
+
+    const interpolatedLeft = sidebarAniValue.interpolate({
+        inputRange: [0, 1],
+        outputRange: ['0%', '60%'],
+    });
+
+    const interpolatedScale = sidebarAniValue.interpolate({
+        inputRange: [0, 1],
+        outputRange: [1.2, 1],
+    });
+
+    const interpolatedOpacity = sidebarAniValue.interpolate({
+        inputRange: [0, 1],
+        outputRange: [0, 1],
+    });
+
+    return {
+        isOpenSideBar,
+        toggleOpenSideBar,
+        interpolatedTop,
+        interpolatedLeft,
+        interpolatedScale,
+        interpolatedOpacity
+    } as const
+}
